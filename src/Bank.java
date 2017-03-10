b875187ef7efbe10a1d98aed6ea108e646a1a57e
Tomasz Gwozdzik <tomasz.gwozdzik@student.put.poznan.pl>
2017-03-10 12:13:47 +0100

    Create template classes

---
+++ src/Bank.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * Created by TG & MM on 10.03.2017.
+ */
+public class Bank {
+    private ArrayList<Object> accountList;
+
+    void addAccount(Object newAccount) {
+       
+    }
+
+
+}
