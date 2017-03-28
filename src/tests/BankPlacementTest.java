542bb0e5c18604fdc7eca3d174cf06be5fe9c735
Tomasz Gwoździk <tomasz.gwozdzik@outlook.com>
2017-03-28 23:59:15 +0200

    Write rest test to Placement class

--- src/tests/BankPlacementTest.java
+++ src/tests/BankPlacementTest.java
@@ -28,7 +28,7 @@ public class BankPlacementTest {
 
             @Override
             public double calculate(double amount, Date startDate, Date endDate) {
-                return amount * 0.05;
+                return endDate.after(startDate) ? amount * 0.1 : amount;
             }
         };
 
@@ -44,15 +44,119 @@ public class BankPlacementTest {
     public void PlacementWrongClientID() throws Exception {
         bank.payment(clientID, accountID, 3000);
         bank.createPlacement("test", accountID, 3000, closeDate, interest);
-
-        assertTrue(false);
     }
 
     @Test (expected = ClientOrProductDoesNotExistException.class)
     public void PlacementWrongAccountID() throws Exception {
         bank.payment(clientID, accountID, 3000);
-        bank.createPlacement("test", accountID, 3000, closeDate, interest);
+        bank.createPlacement(clientID, "test", 3000, closeDate, interest);
+    }
 
-        assertTrue(false);
+    @Test (expected = WrongValueException.class)
+    public void PlacementLessThanZero() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        bank.createPlacement(clientID, accountID, -3000, closeDate, interest);
+    }
+
+    @Test (expected = WrongValueException.class)
+    public void PlacementZero() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        bank.createPlacement(clientID, accountID, 0, closeDate, interest);
+    }
+
+    @Test (expected = NoResourcesException.class)
+    public void PlacementMoreThanHaveOnAccount() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        bank.createPlacement(clientID, accountID, 5000, closeDate, interest);
+    }
+
+    @Test
+    public void PlacementCreate() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        assertNotNull(placementID);
+        assertEquals(1000.0, bank.getAccountBalance(clientID, accountID), 0);
+    }
+
+    @Test
+    public void CheckHistory() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        assertEquals(2, bank.getHistory().getHistory().size(), 0);
+    }
+
+    @Test (expected = ClientOrProductDoesNotExistException.class)
+    public void ClosePlacementWrongClientID() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        bank.closePlacement("test", accountID, placementID, closeDate);
+    }
+
+    @Test (expected = ClientOrProductDoesNotExistException.class)
+    public void ClosePlacementWrongAccountID() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        bank.closePlacement(clientID, "test", placementID, closeDate);
+    }
+
+    @Test (expected = ClientOrProductDoesNotExistException.class)
+    public void ClosePlacementWrongPlacementID() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        bank.closePlacement(clientID, accountID, "test", closeDate);
+    }
+
+    @Test
+    public void ClosePlacementBeforeEndTime() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        double balance = bank.closePlacement(clientID, accountID, placementID, dateFormat.parse("10.02.2017"));
+
+        assertEquals(3000.0, balance, 0);
+    }
+
+    @Test
+    public void ClosePlacementAfterEndTime() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        double balance = bank.closePlacement(clientID, accountID, placementID, dateFormat.parse("30.04.2017"));
+
+        assertEquals(3200.0, balance, 0);
+    }
+
+    @Test
+    public void ClosePlacementAfterEndTimeExact() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        double balance = bank.closePlacement(clientID, accountID, placementID, dateFormat.parse("15.04.2017"));
+
+        assertEquals(3200.0, balance, 0);
+    }
+
+    @Test (expected = ProductIsAlreadyClosedException.class)
+    public void CloseClosedPlacement() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        bank.closePlacement(clientID, accountID, placementID, dateFormat.parse("15.04.2017"));
+        bank.closePlacement(clientID, accountID, placementID, dateFormat.parse("17.04.2017"));
+    }
+
+    @Test
+    public void ClosePlacementHistory() throws Exception {
+        bank.payment(clientID, accountID, 3000);
+        String placementID = bank.createPlacement(clientID, accountID, 2000, closeDate, interest);
+
+        bank.closePlacement(clientID, accountID, placementID, dateFormat.parse("15.04.2017"));
+
+        assertEquals(3, bank.getHistory().getHistory().size(), 0);
     }
 }
